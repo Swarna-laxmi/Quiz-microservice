3ae8da2fc6dbc1c5ecc662ea04e8d2c51fadaab6
swarna <behera.swarnalaxmi@gmail.com>
2025-08-13 09:52:26 -0500

    added retrieve quiz relate classes-Questionwrapper,response

--- src/main/java/com/sb/quizapp/controller/QuizController.java
+++ src/main/java/com/sb/quizapp/controller/QuizController.java
@@ -1,12 +1,14 @@
 package com.sb.quizapp.controller;
 
+import com.sb.quizapp.model.Question;
+import com.sb.quizapp.model.QuestionWrapper;
+import com.sb.quizapp.model.Response;
 import com.sb.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,6 +21,18 @@ public class QuizController {
     public ResponseEntity<String>  createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title)
     {
        return quizService.createQuiz(category,numQ,title);
+    }
+
+    @GetMapping("getQuiz/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(@PathVariable Integer id)
+    {
+      return   quizService.getQuizQuestion(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses)
+    {
+        return quizService.submitQuiz(id,responses);
     }
 
 
